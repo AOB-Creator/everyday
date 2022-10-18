d1f802ee0461ee9be90301e427c8824f5b112ff8
Alpamis01 <alpamisomirbekov@gmail.com>
2022-10-18 23:48:09 +0500

    status

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
 import Navigationbar from "../components/navigationbar";
 import Footer from "../components/footer";
 import "../styles/globals.css";
+import { motion, useScroll, useSpring } from "framer-motion";
 
 function MyApp({ Component, pageProps }) {
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
   return (
     <>
+      <motion.div className="progress-bar" style={{ scaleX }} />
       <Navigationbar />
       <Component {...pageProps} />
       <Footer />
